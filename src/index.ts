4e37e510699ed820e6d7fa7018c1c9ae2b2cf54e
Adalbertus Christopher <adalbertus.chris@gmail.com>
2022-10-04 22:45:18 +0200

    Add index.ts, public_api.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-/* Observables */
-export { asyncPipeFrom } from './observables/async-pipe-from';
-export { asyncPipe } from './observables/async-pipe';
-export { omit } from './observables/omit';
-
-/* Operators */
-export { action } from './operators/action';
-export { awaitAction } from './operators/await-action';
-export { awaitAll } from './operators/await-all';
-
-/* Types */
-export { AwaitAllStrategy } from './operators/await-all'
+export * from './public_api';
